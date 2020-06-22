f7f639f6849fbdcfede741b03abdaa5e0314dd5f
mak627 <m.khadka69@gmail.com>
2020-06-22 11:04:48 +0700

    search node addded and other thread safe changes

--- Multi-Threading/threads_cpp_sample.cpp
+++ Multi-Threading/threads_cpp_sample.cpp
@@ -20,6 +20,7 @@ class singly_linked_list{
 		}
 		void insert_node(int new_val);
 		void delete_node();
+		bool search_node(int val);
 		void show_list();
 };
 
@@ -29,13 +30,15 @@ void singly_linked_list::insert_node(int new_val){
 	new_node->value = new_val;
 	if(head==NULL){
 		new_node->next = NULL;
+		mtx.lock();
 		head = new_node;
 		tail = head;
+		mtx.unlock();
 		new_node = NULL;
 	}
 	else{
-		mtx.lock();
 		new_node->next = head;
+		mtx.lock();
 		head = new_node;
 		mtx.unlock();
 		new_node = NULL;	
@@ -45,46 +48,70 @@ void singly_linked_list::insert_node(int new_val){
 //deletion happens at the beginning of the SLL
 void singly_linked_list::delete_node(){
 	struct node *temp = new node;
-	mtx.lock();
 	temp = head;
+	mtx.lock();
 	head = head->next;
-	delete temp;
 	mtx.unlock();
+	delete temp;
+}
+
+//searching for a node with given value
+bool singly_linked_list::search_node(int val){
+	struct node *temp = head;
+	while(temp!= NULL){
+		cout << temp->value << endl;
+		if(temp->value == val){ // node is found
+			cout << "Node Found" << endl;
+			return true; 
+		}
+		mtx.lock();
+		temp = temp->next;
+		mtx.unlock();	
+	}
+	cout << "Node Not Found" << endl;
+	return false;
 }
 
 //showing the singly linked list
 void singly_linked_list::show_list(){
 	struct node *temp = new node;
 	temp = head;
+	mtx.lock();
 	while(temp != NULL){
 		cout << temp->value << " ";
 		temp = temp->next;	
 	}
+	mtx.unlock();
 	cout << endl;
 }
 
 int main(){
 	
 	auto start = high_resolution_clock::now();
-	
+	int search_val = 3;
 	singly_linked_list sll1;	
-	//Sequential Execution of two functions
-	//sll1.insert_node(1);
-	//sll1.insert_node(2);
-	//sll1.insert_node(3);
-	//sll1.delete_node();
+	
+	//Sequential Execution of three functions
+	/*sll1.insert_node(1);
+	sll1.insert_node(2);
+	sll1.insert_node(3);
+	sll1.search_node(head,search_val);
+	sll1.delete_node();
+	sll1.show_list();*/
 
 	//Parallel Execution of two functions using threads
-	//*****thread safety remains to be implemented*****
 	thread t1(&singly_linked_list::insert_node,&sll1,1);
 	thread t2(&singly_linked_list::insert_node,&sll1,2);
 	thread t3(&singly_linked_list::insert_node,&sll1,3);
-	thread t4(&singly_linked_list::delete_node,&sll1);
+	thread t4(&singly_linked_list::search_node,&sll1,search_val);
+	thread t5(&singly_linked_list::delete_node,&sll1);
+	thread t6(&singly_linked_list::show_list,&sll1);
 	t1.join();
 	t2.join();
 	t3.join();
 	t4.join();
-	sll1.show_list();
+	t5.join();
+	t6.join();
 	
 	auto stop = high_resolution_clock::now();
 	auto time_diff = stop - start;
