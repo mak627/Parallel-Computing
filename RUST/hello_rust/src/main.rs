c7671403dfdfe831f0b4477ab53e027e03806901
mak627 <m.khadka69@gmail.com>
2020-08-21 05:25:41 +0700

    Added code for sequential Prefix Sum

--- RUST/hello_rust/src/main.rs
+++ RUST/hello_rust/src/main.rs
@@ -3,11 +3,15 @@
 use rayon::prelude::*;
 
 fn main() {
-    let input_arr = vec![0,1,2,3];
-    let mut prefix_sum = vec![0,0,0,0];
+    let input_arr: [i32;4] = [0,1,2,3];
+    let mut prefix_sum: [i32;4] = [0,0,0,0];
     prefix_sum[0] = input_arr[0];
+    let n = input_arr.len();
 
-    input_arr.par_iter().for_each(|i|{
+    (1..n).for_each(|i|{
         prefix_sum[i] = prefix_sum[i-1] + input_arr[i];
     });
+    for i in 0..n{
+        println!("{}", prefix_sum[i]);
+    };
 }
