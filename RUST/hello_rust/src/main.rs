858518747425419dce7536bfb1742c0c81efc947
mak627 <m.khadka69@gmail.com>
2020-08-23 05:01:18 +0700

    Sequential Iterator

--- RUST/hello_rust/src/main.rs
+++ RUST/hello_rust/src/main.rs
@@ -7,11 +7,9 @@ fn main() {
     let mut prefix_sum: [i32;4] = [0,0,0,0];
     prefix_sum[0] = input_arr[0];
     let n = input_arr.len();
-
-    (1..n).for_each(|i|{
+    (1..n).into_iter().for_each(|i|{
         prefix_sum[i] = prefix_sum[i-1] + input_arr[i];
     });
-    for i in 0..n{
-        println!("{}", prefix_sum[i]);
-    };
+    
+    println!("{:?}", prefix_sum);
 }
