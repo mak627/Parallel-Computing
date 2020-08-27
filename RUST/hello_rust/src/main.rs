a05df4cc233e4014c771302abb6244eecf51c479
mak627 <m.khadka69@gmail.com>
2020-08-28 06:02:30 +0700

    resolved concurrency issue for mutable variable

--- RUST/hello_rust/src/main.rs
+++ RUST/hello_rust/src/main.rs
@@ -1,5 +1,6 @@
 //mod print;
 //mod vars;
+use std::sync::{Arc, Mutex};
 use rayon::prelude::*;
 
 fn main() {
@@ -7,8 +8,11 @@ fn main() {
     let mut prefix_sum: [i32;4] = [0,0,0,0];
     prefix_sum[0] = input_arr[0];
     let n = input_arr.len();
-    (1..n).into_iter().for_each(|i|{
-        prefix_sum[i] = prefix_sum[i-1] + input_arr[i];
+    let prefix_sum = Mutex::new(prefix_sum);
+    (1..n).into_par_iter().for_each(|i|{
+        let mut prefix_sum = prefix_sum.lock().unwrap();
+        let val = prefix_sum[i-1] + input_arr[i];
+        prefix_sum[i] = val;
     });
     
     println!("{:?}", prefix_sum);
